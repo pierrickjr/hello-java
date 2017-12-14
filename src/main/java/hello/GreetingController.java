399a551a3da98a44d22682c5e08e48f71d32af38
Pierrick <pierrickjr@gmail.com>
2017-12-14 18:32:19 +0100

    import GreetingTranslator

--- src/main/java/hello/GreetingController.java
+++ src/main/java/hello/GreetingController.java
@@ -4,6 +4,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import hello.GreetingTranslator;
 
 @Controller
 public class GreetingController {
@@ -19,7 +20,7 @@ public class GreetingController {
     public String greeting(@RequestParam(value="name", required=false, defaultValue="World") String name,
                @RequestParam(value="lang", required=false, defaultValue="en") String lang,
                Model model) {
-                 model.addAttribute("hello", greetingTranslator.sayHelloIn(lang));
+                 model.addAttribute("hello", GreetingTranslator.sayHelloIn(lang));
                  model.addAttribute("name", name);
                  return "greeting";
   }
