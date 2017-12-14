a34303f665f35b5e99ce559015e3bee611219abc
Pierrick <pierrickjr@gmail.com>
2017-12-14 18:28:25 +0100

    static metodo

--- src/main/java/hello/GreetingController.java
+++ src/main/java/hello/GreetingController.java
@@ -8,12 +8,12 @@ import org.springframework.web.bind.annotation.RequestParam;
 @Controller
 public class GreetingController {
 
-    @RequestMapping("/greeting")
+    /*@RequestMapping("/greeting")
     public String greeting(@RequestParam(value="name", required=false, defaultValue="World") String name,
     					   Model model) {
                    model.addAttribute("name", name);
                    return "greeting";
-    }
+    }*/
 
     @RequestMapping("/greeting")
     public String greeting(@RequestParam(value="name", required=false, defaultValue="World") String name,
